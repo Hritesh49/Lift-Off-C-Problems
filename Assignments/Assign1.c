4d6dfc04b6afb5b70f511f635752a84b930a158a
Hritesh49 <127621507+Hritesh49@users.noreply.github.com>
2023-03-12 00:11:37 +0530

    Upload Assignment 1

--- Assignments/Assign1.c
+++ Assignments/Assign1.c
@@ -2,11 +2,14 @@
 
 int main(){
     for(int i=1;i<=100;i++){
-        if(i%3==0){
+        if(i%3==0 && i%5!=0){
             printf("\nFizz");
         }
-        else if(i%5==0){
+        else if(i%5==0 && i%3!=0){
             printf("\nBuzz");
+        }
+        else if(i%3==0 && i%5==0){
+            printf("\nFizz Buzz");
         }
         else printf("\n%d",i);
     }
